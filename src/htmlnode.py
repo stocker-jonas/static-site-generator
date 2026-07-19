e4ef7c61914015301d8f88f62383d42d55039e42
stocker-jonas <stockerjonas.sj@gmail.com>
2026-07-19 15:28:31 +0200

    struggling...

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,8 +20,56 @@ class HTMLNode():
         if self.props is None:
             return str_output
 
-        for i in range(len(props) + 1):
-            str_output += f" {props.item()[0]}={props.item()[1]}"
+        for i in range(len(self.props) + 1):
+            str_output += f" {self.props.items()[0]}={self.props.items()[1]}"
 
-    def __repr__(self) -> None:
+    def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(
+            self,
+            tag: str | None,
+            value: str,
+            props: dict[str, str] | None = None) -> None:
+
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("Leaf Nodes must have a value assigned!")
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(
+            self,
+            tag: str,
+            children: list["HTMLNode"],
+            props: dict[str, str] | None = None) -> None:
+        super().__init__(tag, None, children, props)
+
+    def to_html(self) -> str:
+        if self.tag is None:
+            raise ValueError("Parent Nodes must have a tag assigned!")
+
+        if self.children is None:
+            raise ValueError("Parent Nodes must have at least one child assigned!")
+
+        return f"<{self.tag}{self.props_to_html()}>{self.print_children()}</{self.tag}>"
+
+    def print_children(self) -> str:
+        str = ""
+        if self.children is None:
+            return str
+
+        for child in self.children:
+            str += child.to_html()
+
+        return str
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
@@ -19,3 +19,36 @@ class TestHTMLNode(unittest.TestCase):
         
         self.assertNotEqual("HTMLNode(p, None, children: 1, 1)",
                         repr(node))
+
+    
+
+    def test_eq3(self):
+        node = LeafNode("p", "This is a paragraph", {"a": "idk"})
+
+        self.assertEqual("LeafNode(p, This is a paragraph, {'a': 'idk'})",
+                         repr(node))
+
+    def test_eq4(self):
+        node = ParentNode(
+                "h1",
+                [
+                    ParentNode(
+                        "p",
+                        [
+                            LeafNode(
+                                "b",
+                                "bold",
+                                {"x": "y"}
+                            )
+                        ],
+                        {"z": "a"}
+                    ),
+                    LeafNode(
+                        "i",
+                        "italic"
+                    )
+                ]
+            )
+
+        self.assertEqual("<h1><p z=a><b x=y>bold</b><i>italic</i></h1>",
+                         node.to_html())
